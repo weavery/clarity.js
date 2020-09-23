8f9c70e46947581b80bf51e2262f2edc75c6afc0
Arto Bendiken <arto@bendiken.net>
2020-09-24 01:11:14 +0300

    Configured the test suite using Jest.

---
+++ clarity.test.js
@@ -0,0 +1,7 @@
+/* This is free and unencumbered software released into the public domain. */
+
+import * as clarity from './clarity.js';
+
+test('blockHeight returns an uint', () => {
+  expect(clarity.blockHeight()).toBe(0);  // TODO
+});
