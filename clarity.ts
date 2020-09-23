2eb49140edbd942823d87c8122e4becf787e1ca1
Arto Bendiken <arto@bendiken.net>
2020-09-23 16:40:06 +0300

    Removed unnecessary semicolons.

--- clarity.ts
+++ clarity.ts
@@ -32,173 +32,173 @@ export function requireFeature(feature: string): void {
  * @link https://docs.blockstack.org/references/language-clarity#as-contract
  */
 export function asContract<A>(expr: expr<A>): A {
-  return expr(); // TODO
+  return expr() // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#at-block
  */
 export function atBlock<A>(blockHash: buff, expr: expr<A>): A {
-  return expr(); // TODO
+  return expr() // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#block-height
  */
 export function blockHeight(): uint {
-  return 0; // TODO
+  return 0 // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#contract-call
  */
 export function contractCall<A, B>(contractName: trait, functionName: string, ...args: any): response<A, B> {
-  return err(null); // TODO
+  return err(null) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#contract-caller
  */
 export function contractCaller(): principal {
-  return ""; // TODO
+  return "" // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#contract-of
  */
 export function contractOf(contractName: trait): principal {
-  return ""; // TODO
+  return "" // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#err
  */
 export function err<T>(value: T) {
-  return new Err(value);
+  return new Err(value)
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#ft-get-balance
  */
 export function ftGetBalance(tokenName: string, principal: principal): uint {
-  return 0; // TODO
+  return 0 // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#ft-mint
  */
 export function ftMint(tokenName: string, amount: uint, recipient: principal): response<bool, uint> {
-  return err(null); // TODO
+  return err(null) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#ft-transfer
  */
 export function ftTransfer(tokenName: string, amount: uint, sender: principal, recipient: principal): response<bool, uint> {
-  return err(null); // TODO
+  return err(null) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#get-block-info
  */
 export function getBlockInfo(propName: string, blockHeight: uint): optional<buff> | optional<uint> {
-  return null; // TODO
+  return null // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#hash160
  */
 export function hash160(value: buff | uint | int): buff {
-  return new ArrayBuffer(20); // TODO
+  return new ArrayBuffer(20) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#keccak256
  */
 export function keccak256(value: buff | uint | int): buff {
-  return new ArrayBuffer(32); // TODO
+  return new ArrayBuffer(32) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#nft-get-owner
  */
 export function nftGetOwner(assetClass: string, assetID: string): optional<principal> {
-  return null; // TODO
+  return null // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#nft-mint
  */
 export function nftMint(assetClass: string, assetID: string, recipient: principal): response<bool, uint> {
-  return err(null); // TODO
+  return err(null) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#nft-transfer
  */
 export function nftTransfer(assetClass: string, assetID: string, sender: principal, recipient: principal): response<bool, uint> {
-  return err(null); // TODO
+  return err(null) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#sha256
  */
 export function sha256(value: buff | uint | int): buff {
-  return new ArrayBuffer(32); // TODO
+  return new ArrayBuffer(32) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#sha512
  */
 export function sha512(value: buff | uint | int): buff {
-  return new ArrayBuffer(64); // TODO
+  return new ArrayBuffer(64) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#sha512256
  */
 export function sha512_256(value: buff | uint | int): buff {
-  return new ArrayBuffer(32); // TODO
+  return new ArrayBuffer(32) // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#try
  */
 export function tryUnwrap<A, B>(optionInput: optional<A> | response<A, B>): A {
-  throw new Error("not implemented yet");  // TODO
+  throw new Error("not implemented yet")  // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#tx-sender
  */
 export function txSender(): principal {
-  return ""; // TODO
+  return "" // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#unwrap
  */
 export function unwrap<A, B>(optionInput: optional<A> | response<A, B>, thrownValue: A): A {
-  return thrownValue;  // TODO
+  return thrownValue  // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#unwrap-err
  */
 export function unwrapErr<A, B>(responseInput: response<A, B>, thrownValue: B): B {
-  return thrownValue;  // TODO
+  return thrownValue  // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#unwrap-err-panic
  */
 export function unwrapErrPanic<A, B>(responseInput: response<A, B>): B {
-  throw new Error("not implemented yet");  // TODO
+  throw new Error("not implemented yet")  // TODO
 }
 
 /**
  * @link https://docs.blockstack.org/references/language-clarity#unwrap-panic
  */
 export function unwrapPanic<A, B>(optionInput: optional<A> | response<A, B>): A {
-  throw new Error("not implemented yet");  // TODO
+  throw new Error("not implemented yet")  // TODO
 }
